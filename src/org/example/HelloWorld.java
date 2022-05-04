f0d38d86772922584a89145b8327af284cc6e65c
Ishita Patel <alka@ishitas-macbook-air-2.local>
2022-05-03 21:57:13 -0400

    Initial commit

---
+++ src/org/example/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class HelloWorld { // accessor keyword identifier
+    public static void main(String[] args) {//command line agrument
+        int a;
+        a = 2;
+        System.out.println("Hello World" + a); //class object method argument
+    }
+}
---
+++ src/org/example/MethodExamples.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class MethodExamples {
+    public static void main(String[] args) {
+
+    }
+
+    void method(){
+
+    }
+
+    int method1(){
+        return 0;
+    }
+
+    static String method3(){
+        return null;
+    }
+
+}
